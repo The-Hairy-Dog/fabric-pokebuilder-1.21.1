39ca1b13994189a90165ba9742a1df17e431e9a4
Will <willtcooley@gmail.com>
2025-09-24 21:31:54 +1000

    Initial commit

---
+++ src/main/java/net/will/pokebuildermod/PokeBuilderModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.will.pokebuildermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PokeBuilderModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
